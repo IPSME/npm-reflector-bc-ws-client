9193d61cf0a74ef4b78630850583e763c05eb9a9
knev <knev@users.noreply.github.com>
2022-04-25 21:40:22 +0200

    Use connection resolvers with Service Worker state to delay messages until setup complete.

--- msgenv-broadcast-channel.js
+++ msgenv-broadcast-channel.js
@@ -1,7 +1,12 @@
 
-const msgenv_BroadcastChannel = new BroadcastChannel('');
+// var global_subscription_handler= null;
+var global_ServiceWorker= null;
 
-var global_subscription_handler= null;
+//-------------------------------------------------------------------------------------------------
+// MsgEnv
+
+console.log('MsgEnv: new BroadcastChannel()');
+const msgenv_BroadcastChannel = new BroadcastChannel('');
 
 function IPSME_MsgEnv_subscribe(handler) {
     window.global_subscription_handler= handler;
@@ -11,18 +16,56 @@ function IPSME_MsgEnv_unsubscribe(handler) {
     window.global_subscription_handler= null;
 }
 
+// https://stackoverflow.com/questions/23051416/uncaught-invalidstateerror-failed-to-execute-send-on-websocket-still-in-co
+//
+let connection_resolvers= [];
+let checkConnection = () => {
+    return new Promise((resolve, reject) => {
+        if (global_ServiceWorker) {
+            if (global_ServiceWorker === "activated")
+                resolve();
+        }
+        else {
+            connection_resolvers.push({resolve, reject});
+        }
+    });
+}
+
 msgenv_BroadcastChannel.onmessage = event => {
+    msg= event.data;
+    console.log('MsgEnv: onmessage: ['+ msg +']');
+
 	// console.log('msgenv_BroadcastChannel.onmessage: ['+ event.data +']'); 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/null
     if (global_subscription_handler !== null)
-        window.global_subscription_handler(event.data);
+        window.global_subscription_handler(msg);
 }
 
-function IPSME_MsgEnv_publish(str_msg) {
+async function async_publish(str_msg) {
+    await checkConnection();
+
+    console.log('MsgEnv: async_publish ['+ str_msg +']');
     msgenv_BroadcastChannel.postMessage(str_msg);
 }
 
+function IPSME_MsgEnv_publish(str_msg) {
+	async_publish(str_msg);
+}
+
 //-------------------------------------------------------------------------------------------------
+
+// https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+//
+function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+  
+console.log(uuidv4());
+
+//-------------------------------------------------------------------------------------------------
+// js-reflector-ws-client (ServiceWorker)
 
 // https://web.dev/service-worker-lifecycle/
 // You can detect if a client is controlled via navigator.serviceWorker.controller which will be null or a service worker instance
@@ -39,23 +82,24 @@ if ('serviceWorker' in navigator)
     navigator.serviceWorker.register('reflector-ws-client.js').then(function(registration) 
     {
         // manually update service worker
-        // registration.update();
+        //registration.update();
 
-        registration.installing; // the installing worker, or undefined
-        registration.waiting; // the waiting worker, or undefined
-        registration.active; // the active worker, or undefined
+        // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorker
 
-        registration.addEventListener('updatefound', function() {
-            // If updatefound is fired, it means that there's
-            // a new service worker being installed.
-            var installingWorker = registration.installing;
-            console.log('REG: A new service worker is being installed:', installingWorker);
+        if (registration.installing) {
+            global_ServiceWorker= registration.installing;
+        }
+        // else if (registration.waiting) {
+        //     console.log('REG: '+ global_ServiceWorker.state);
+        // }
+        // else if (registration.active) {
+        //     console.log('REG: '+ global_ServiceWorker.state);
+        // }
 
-            // You can listen for changes to the installing service worker's
-            // state via installingWorker.onstatechange
-
-            // https://web.dev/service-worker-lifecycle/
-            registration.installing.state;
+        if (global_ServiceWorker) 
+        {
+            // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorker/state
+            // ServiceWorker.state;
             // "installing" - the install event has fired, but not yet complete
             // "installed"  - install complete
             // "activating" - the activate event has fired, but not yet complete
@@ -63,10 +107,19 @@ if ('serviceWorker' in navigator)
             // "redundant"  - discarded. Either failed install, or it's been
             //                replaced by a newer version        
 
-            registration.installing.addEventListener('statechange', () => {
-                // newWorker.state has changed
-            });
-        });
+            global_ServiceWorker.onstatechange= onstatechange= function(event) {
+                if (global_ServiceWorker.state === "activated") {
+                    connection_resolvers.forEach(r => r.resolve());
+                }
+            }
+        }
+
+        registration.onupdatefound= function(event) 
+        {
+            // If updatefound is fired, it means that there's
+            // a new service worker being installed.
+            console.log('REG: A new service worker is being installed:', registration.installing);
+        };
 
     })
     .catch(function(error) {
@@ -82,3 +135,5 @@ if ('serviceWorker' in navigator)
 else {
     console.log('REG: Service workers are not supported.');
 }
+
+//-------------------------------------------------------------------------------------------------
