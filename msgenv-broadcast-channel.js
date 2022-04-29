a6ca7e475164c08ee3a9e2e53e4ead37ba5cff28
knev <knev@users.noreply.github.com>
2022-04-29 17:39:27 +0200

    Move utility function to top.

--- msgenv-broadcast-channel.js
+++ msgenv-broadcast-channel.js
@@ -3,6 +3,18 @@
 var global_ServiceWorker= null;
 
 //-------------------------------------------------------------------------------------------------
+
+// https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+//
+function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+  
+// console.log(uuidv4());
+
+//-------------------------------------------------------------------------------------------------
 // MsgEnv
 
 console.log('MsgEnv: new BroadcastChannel()');
@@ -52,18 +64,6 @@ async function async_publish(str_msg) {
 function IPSME_MsgEnv_publish(str_msg) {
 	async_publish(str_msg);
 }
-
-//-------------------------------------------------------------------------------------------------
-
-// https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
-//
-function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
-  }
-  
-console.log(uuidv4());
 
 //-------------------------------------------------------------------------------------------------
 // js-reflector-ws-client (ServiceWorker)
