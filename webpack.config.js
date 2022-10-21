17cca0ffe0164fb374e33f1643a9a1a2db48b160
knev <knev@users.noreply.github.com>
2022-10-21 09:57:09 +0200

    Use webpack to copy sharedworker-reflector, but in correct dirs.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode : "production",
@@ -7,4 +8,13 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'reflector-bc-ws-client.js',
     },
+	plugins: [
+		// https://stackoverflow.com/questions/55420795/copy-files-from-node-modules-to-dist-dir
+		new CopyWebpackPlugin({
+			patterns: [{
+				from: 'src/sharedworker-reflector.js',
+				to: 'sharedworker-reflector.es.js'
+			}]
+		})
+	]
 };
